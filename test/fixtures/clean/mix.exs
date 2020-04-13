e3fda011f43392f6091e5a46a9ead93bdb3eb08a
Łukasz Niemier <lukasz@niemier.pl>
2020-04-14 00:36:39 +0200

    ft: migrate to compiler tracers
    - CLI options
    - assuming that callbacks are used
    - using ignore list
    - print out diagnostics
    - create manifest containing informations about used functions

---
+++ test/fixtures/clean/mix.exs
@@ -0,0 +1,11 @@
+defmodule MixUnused.Fixtures.CleanProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :clean,
+      compilers: [:unused | Mix.compilers()],
+      version: "0.0.0"
+    ]
+  end
+end
--- test/mix_unused_test.exs
+++ test/mix_unused_test.exs
@@ -1,11 +1,86 @@
 defmodule MixUnusedTest do
-  use ExUnit.Case
+  use MixUnused.Case, async: false
 
-  alias Mix.Tasks.Unused, as: Subject
+  import ExUnit.CaptureIO
 
-  doctest Subject
+  describe "umbrella" do
+    test "simple file" do
+      in_fixture("umbrella", fn ->
+        assert {{:ok, diagnostics}, _} = run(:umbrella, "compile")
 
-  test "greets the world" do
-    assert Subject.run(["--quiet"]) == :ok
+        assert has_diagnostics_for?(diagnostics, ModuleA, :foo, 0)
+        assert has_diagnostics_for?(diagnostics, ModuleB, :bar, 0)
+      end)
+    end
+  end
+
+  describe "clean" do
+    test "behaviours should not be reported" do
+      in_fixture("clean", fn ->
+        assert {{:ok, []}, _} = run(:clean, "compile")
+      end)
+    end
+  end
+
+  describe "unclean" do
+    test "ignored function is not reported" do
+      in_fixture("unclean", fn ->
+        assert {{:ok, diagnostics}, output} = run(:unclean, "compile")
+
+        refute has_diagnostics_for?(diagnostics, Foo, :bar, 0)
+        refute output =~ "Foo.bar/0 is unused"
+      end)
+    end
+
+    test "unused function is reported" do
+      in_fixture("unclean", fn ->
+        assert {{:ok, diagnostics}, output} = run(:unclean, "compile")
+
+        assert has_diagnostics_for?(diagnostics, Foo, :foo, 0)
+        assert output =~ "Foo.foo/0 is unused"
+      end)
+    end
+  end
+
+  describe "two mods" do
+    test "when recompiling it inform about unused module" do
+      in_fixture("two_mods", fn ->
+        assert {{:ok, diagnostics}, output} = run(:two_mods, "compile")
+        assert has_diagnostics_for?(diagnostics, Foo, :foo, 0)
+        refute has_diagnostics_for?(diagnostics, Foo, :bar, 0), output
+
+        Mix.Task.clear()
+
+        content =
+          File.read!("lib/foo.ex")
+          |> String.replace("dummy", "dummer")
+
+        File.write!("lib/foo.ex", content)
+
+        assert {{:ok, diagnostics}, _} = run(:two_mods, "compile")
+        assert has_diagnostics_for?(diagnostics, Foo, :foo, 0)
+        refute has_diagnostics_for?(diagnostics, Foo, :bar, 0)
+      end)
+    end
+  end
+
+  defp run(project, task) do
+    Mix.Project.in_project(project, ".", fn _ ->
+      captured =
+        capture_io(fn ->
+          send(self(), {:task, Mix.Task.run(task)})
+        end)
+
+      send(self(), {:io, captured})
+    end)
+
+    assert_received {:task, result}
+    assert_received {:io, output}
+
+    {result, output}
+  end
+
+  defp has_diagnostics_for?(diagnostics, m, f, a) do
+    Enum.any?(diagnostics, & &1.message =~ "#{inspect(m)}.#{f}/#{a}")
   end
 end
