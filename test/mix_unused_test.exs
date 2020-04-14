5782623d77dd8810e953d06fa1d6e9ff6745c0b7
Łukasz Niemier <lukasz@niemier.pl>
2020-04-14 10:33:39 +0200

    test: basic test for umbrella projects

--- test/mix_unused_test.exs
+++ test/mix_unused_test.exs
@@ -1,11 +1,29 @@
 defmodule MixUnusedTest do
   use MixUnused.Case
 
-  test "simple file" do
-    in_fixture("umbrella", fn ->
-      Mix.Project.in_project(:umbrella, ".", fn _ ->
-        assert nil = Mix.Task.run("compile")
+  describe "umbrella" do
+    test "simple file" do
+      in_fixture("umbrella", fn ->
+        Mix.Project.in_project(:umbrella, ".", fn _ ->
+          assert {:ok, diagnostics} = Mix.Task.run("compile")
+
+          assert %Mix.Task.Compiler.Diagnostic{
+            compiler_name: "unused",
+            severity: :hint,
+            position: nil,
+            file: "unknown",
+            message: "ModuleA.foo/0 is unused"
+          } in diagnostics
+
+          assert %Mix.Task.Compiler.Diagnostic{
+            compiler_name: "unused",
+            severity: :hint,
+            position: nil,
+            file: "unknown",
+            message: "ModuleB.bar/0 is unused"
+          } in diagnostics
+        end)
       end)
-    end)
+    end
   end
 end
